bac9d01a7b5173dab727f83507488fcecd50c3af
swetha <swetha.valambath@gmail.com>
2022-09-15 10:50:56 -0500

    samplecode

---
+++ application/src/main/java/com/okta/preauthorize/application/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.okta.preauthorize.application;
+  
+
+import org.springframework.context.annotation.Configuration;  
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;  
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;  
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;  
+  
+@Configuration  
+@EnableGlobalMethodSecurity(prePostEnabled = true)  
+public class SecurityConfig extends WebSecurityConfigurerAdapter {  
+    protected void configure(final HttpSecurity http) throws Exception {  
+        http.antMatcher("/**")  
+            .authorizeRequests()  
+            .antMatchers("/").permitAll()  
+            .anyRequest().authenticated()  
+            .and().oauth2Login(); 
+    }  
+}
---
+++ application/src/main/java/com/okta/preauthorize/application/WebController.java
@@ -0,0 +1,24 @@
+package com.okta.preauthorize.application;
+
+ 
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.ResponseBody;  
+@Controller  
+public class WebController {  
+  
+    @RequestMapping("/")  
+    @ResponseBody  
+    public String home() {  
+        return "Welcome home!";  
+    }  
+  
+    @RequestMapping("/restricted")  
+    @ResponseBody  
+    public String restricted() {  
+        return "You found the secret lair!";  
+    }  
+  
+}
